556130e55334babec1cfbe03e3c10a6bed3a1f1a
Merlito Quino Jr <merljustine@gmail.com>
2023-08-30 20:53:32 +0800

    Add New Lesson

---
+++ 15-javascript_fundamentals/main.js
@@ -0,0 +1,31 @@
+// let num1 = 23;
+// let num2 = 97;
+// let sum = num1 + num2;
+// console.log(sum);
+
+console.log(23 + 97);
+
+// let seqNumbers = [3, 6, 9, 12, 15, 30];
+
+let num1 = 4;
+let num2 = 6;
+let num3 = 9;
+let divisor = 77;
+
+console.log((num1 + num2 + num3) / divisor);
+
+var a = 10;
+console.log(a);
+
+var result = 9 * a;
+console.log(result);
+
+var b = 7 * a;
+console.log(b);
+
+const maxNum = 57;
+let minNum = 13;
+let actualMaxNum = maxNum - minNum;
+
+let percentage = actualMaxNum / maxNum;
+console.log(percentage);
