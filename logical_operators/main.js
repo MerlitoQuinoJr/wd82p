da839f64023a2ecbab05eaf09c218bdf9b34083e
Merlito Quino Jr <merljustine@gmail.com>
2023-09-04 21:12:40 +0800

    Problem Solving-Loop

---
+++ logical_operators/main.js
@@ -0,0 +1,45 @@
+// function subtractNumber(num1, num2) {
+//   console.log(num1 - num2);
+// }
+// subtractNumber(1, 2);
+// subtractNumber(2, 2);
+// subtractNumber(4, 2);
+
+// const subtract = (num1, num2) => num1 - num2;
+// console.log(subtract(1, 2));
+// console.log(subtract(4, 2));
+
+// function inputFullName(firstname, lastname) {
+//   return firstname + " " + lastname;
+// }
+
+// console.log(inputFullName("Carlo", "Dacuyan"));
+// console.log(inputFullName("Jose", "Rizal"));
+// console.log(inputFullName("Pedro", "Laura"));
+
+function add7(number) {
+  return number + 7;
+}
+console.log(add7(1));
+console.log(add7(7));
+console.log(add7(5));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(4, 4));
+console.log(multiply(5, 5));
+
+function capitalizedText(text) {
+  return text.toUpperCase();
+}
+
+console.log(capitalizedText("try"));
+console.log(capitalizedText("merry Christmas"));
+console.log(capitalizedText("happY NeW Year"));
+
+function movedLastLetter(letter) {
+  return letter.split("");
+}
+console.log(movedLastLetter("try"));
---
+++ problem_solving_loop/main.js
@@ -0,0 +1,13 @@
+let userInput = parseInt(prompt("Please Input a Number"));
+
+for (let i = 1; i <= userInput; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
